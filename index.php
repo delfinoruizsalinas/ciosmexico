393b8981a012ee7e68c26a3e8128a92c90a4e011
druiz <druiz@pc-24950.quimica-apollo.com.mx>
2022-07-11 16:08:51 -0500

    api jss

--- index.php
+++ index.php
@@ -226,7 +226,39 @@ Escríbenos y en breve nos pondremos en contacto contigo.</p>
         url: 'http://178.62.220.4:1337/api/partners?populate=imagen',
         dataType: 'json',
         success: function(data) {
-          console.log(data)
+          
+
+          $.each(data.data, function(key, value){
+            link = "";
+            /*if(!value[key]['attributes'].link_pange.length){
+              link = '#';
+            }else{
+              link = value[key].attributes.link_pange;                        
+            }*/
+            console.log(value.attributes.link_pange);
+          });
+
+   
+/*
+          $url ="";
+          $link = "";
+          $i = $key;
+
+          if(empty($value->attributes->link_pange)){
+              $link = '#';
+          }else{
+              $link = $value->attributes->link_pange;                        
+          }
+          foreach($value->attributes->imagen as $item){
+
+                  if(empty($item->attributes->formats->thumbnail)){
+                      $url = 'http://178.62.220.4:1337'.$item->attributes->url;
+                  }else{
+                      $url = 'http://178.62.220.4:1337'.$item->attributes->formats->thumbnail->url;                               
+                  }
+          }       
+          echo '<div class="col-sm-6 col-lg-3"><a class="box-sponsor wow fadeInUp" target="_blank" href="'.$link.'" data-wow-delay="1.'.$i.'s"><img src="'.$url.'" alt="" width="120" height="119"/></a></div>';
+          */
         }
       });
     </script>
