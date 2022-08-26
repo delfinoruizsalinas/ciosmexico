c53106313b6fade9cf312210fce985db2d572342
druiz <druiz@pc-24950.quimica-apollo.com.mx>
2022-08-26 00:21:32 -0500

    noticias

--- api_block_headers_share.php
+++ api_block_headers_share.php
@@ -10,7 +10,7 @@
         // In the case of this input, do key and array lookups to get the values
 
         //echo $obj->data->attributes->imagen->data->attributes->formats->thumbnail->url;
-        echo '<meta property="og:url" content="eventos-detalle.php?id='.$idbiog.'" />
+        echo '<meta property="og:url" content="eventos-detalle.php?id='.$idbiog.'&evento=presencial" />
         <meta property="og:type" content="Sitio Web" />
         <meta property="og:title" content="'.$obj->data->attributes->titulo.'" />
         <meta property="og:image" content="'.$url.$obj->data->attributes->imagen->data->attributes->url.'" />';
