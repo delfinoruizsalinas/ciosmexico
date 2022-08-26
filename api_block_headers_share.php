2acf13d08607627ce971df9416295a0306e0125e
druiz <druiz@pc-24950.quimica-apollo.com.mx>
2022-08-26 00:48:21 -0500

    add noticias

--- api_block_headers_share.php
+++ api_block_headers_share.php
@@ -11,7 +11,7 @@
 
         //echo $obj->data->attributes->imagen->data->attributes->formats->thumbnail->url;
         echo '<meta property="og:url" content="eventos-detalle.php?id='.$idbiog.'&evento=presencial" />
-        <meta property="og:type" content="Sitio Web" />
+        <meta property="og:type" content="Cios Mexicanos - Eventos" />
         <meta property="og:title" content="'.$obj->data->attributes->titulo.'" />
         <meta property="og:image" content="'.$url.$obj->data->attributes->imagen->data->attributes->url.'" />';
     }
@@ -23,7 +23,7 @@
         //foreach($value->attributes->imagen as $item){
          
         echo '<meta property="og:url" content="noticias-detalle.php?id='.$idbiog.'&evento=noticia" />
-        <meta property="og:type" content="Sitio Web" />
+        <meta property="og:type" content="Cios Mexicanos - Noticias" />
         <meta property="og:title" content="'.$obj->data->attributes->titulo.'" />
         <meta property="og:image" content="'.$url.$obj->data->attributes->imagen->data[0]->attributes->formats->small->url.'" />';
       }                                                                        
