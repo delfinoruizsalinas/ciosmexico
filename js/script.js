826dabf9e1cce60351090fc32f96208d18e76cc5
Delfino Ruiz <druiz@apollo.mx>
2022-11-29 11:49:23 -0600

    partners v5

--- js/script.js
+++ js/script.js
@@ -1587,11 +1587,6 @@
 		}
 		var owl = $('.owl-carousel');
 		owl.owlCarousel({
-			items:4,
-			loop:true,
-			margin:10,
-			autoplay:true,
-			autoplayTimeout:1000,
 			autoplayHoverPause:true
 		});
 	});
